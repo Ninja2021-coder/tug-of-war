937d2b46c7ede9e2d38b0aebe65be8d6d162e44c
Ninja2021-coder <79054670+Ninja2021-coder@users.noreply.github.com>
2021-06-04 16:07:57 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+var canvas=document.querySelector('canvas')
+var ctx=canvas.getContext('2d')
+
+var middle=250
+var position=middle
+
+function draw(){
+    ctx.clearRect(0,0,500,100)
+
+    ctx.beginPath()
+    ctx.moveTo(middle,0)
+    ctx.lineTo(middle,100)
+    ctx.stroke()
+
+    ctx.beginPath()
+    ctx.fillStyle='cyan'
+    ctx.arc(position,50,5,0,Math.PI*3)
+    ctx.fill()
+
+
+}
+
+draw()
+
+function move(distance){
+    position=position+distance
+    draw()
+}
+
+window.addEventListener('keydown',function(event){event.preventDefault()
+
+    if(event.key==='ArrowRight'){
+        move(10)
+    }
+    if(event.key==='ArrowLeft'){
+        move(-10)
+    }
+})
+
+function checkWinner(){
+    if(position>middle){
+        alert("Right wins!")
+    }
+    if(position<middle){
+        alert("Leftt wins!")
+    }
+    if(position===middle){
+        alert("It's a draw!")
+    }
+}
+
+setTimeout(checkWinner,10000)
